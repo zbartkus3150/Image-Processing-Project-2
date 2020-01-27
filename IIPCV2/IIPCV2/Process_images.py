e09f909f7fc9a7ec445c6ecd715b3f96ce12c6a5
zbartkus <zbartkuss3150@gmail.com>
2020-01-27 11:47:03 +0100

    comments to code

--- IIPCV2/IIPCV2/Process_images.py
+++ IIPCV2/IIPCV2/Process_images.py
@@ -6,7 +6,7 @@ import glob
 import shutil
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
 from skimage import feature
@@ -14,9 +14,7 @@ from skimage import feature
 import warnings
 warnings.filterwarnings('ignore')
 
-cv2.imshow("test", cv2.imread("../../isolated/negundo/l38.jpg"))
-cv2.waitKey()
-
+#variables declaration
 train_path = '../../isolated'
 bins = 8
 num_trees = 100
@@ -56,31 +54,25 @@ def fd_loc_bin_patt(image, numPoints=24, radius=8, eps=1e-7):
 def fd_zernike(image):
      return mahotas.features.zernike_moments(image, min(image.shape))
 
-# feature-descriptor-6: SIFT
-def fd_SIFT(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #freakExtractor = cv2.xfeatures2d.FREAK_create()
-    #sift = cv2.xfeatures2d.SIFT_create()
-    #keypoints, descriptors = sift.detectAndCompute(image,None)
-    #keypoints,descriptors= freakExtractor.compute(image,None)
-    kaze = cv2.KAZE_create()
-    keypoints, descriptors = kaze.detectAndCompute(image, None)
-    return descriptors
 
 def SegmentImage(path, debug):
 
+    #read image
     image = cv2.imread(path,cv2.IMREAD_COLOR)
 
+    #change color palette and create a mask containing specific color range
     hsv = cv2.cvtColor(image , cv2.COLOR_BGR2HSV) 
     low = np.array([0, 0, 0],np.uint8) 
     high = np.array([180, 255, 165],np.uint8)
     mask = cv2.inRange(hsv, low , high)
 
+    #compute contours and keep only the largest one
     ret,thresh = cv2.threshold(mask, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cont = sorted(contours, key = cv2.contourArea, reverse = True)[0]
     mask = cv2.drawContours(np.zeros((image.shape[0], image.shape[1], 1), np.uint8), [cont], 0, (255,255,255), cv2.FILLED)
 
+    #create segmented image using bitwise AND operation
     segmented = cv2.bitwise_and(image , image , mask=mask) 
     ret, mask = cv2.threshold(mask, 127, 255, 0)
 
@@ -98,14 +90,15 @@ train_labels = os.listdir(train_path)
 train_labels.sort()
 
 global_features = []
-labels          = []
+labels = []
 
 
-# loop over the training data sub-folders
+# iterate over species folders
 for training_name in train_labels:
     dir = os.path.join(train_path, training_name)
     current_label = training_name
 
+    #iterate over all images in folder
     for x in range(1,len([im for im in os.listdir(dir)])+1):
         # get the image file name
         if x < 10:
@@ -113,17 +106,18 @@ for training_name in train_labels:
         else:
             file = dir + "/l" +  str(x) + ".jpg"
 
+        #read image and get it's mask and segmented version
         image = cv2.imread(file)
         image_seg, image_mask = SegmentImage(file, False)
-
-        #fv_hu_moments = fd_hu_moments(image_mask)
-        #fv_haralick   = fd_haralick(image_seg)
+        
+        #get all features descriptors
+        fv_hu_moments = fd_hu_moments(image_mask)
+        fv_haralick   = fd_haralick(image_seg)
         fv_histogram  = fd_histogram(image, None)
         fv_loc_bin_patt = fd_loc_bin_patt(image, 24, 4)
-        #fv_zernike     = fd_zernike(image_mask)
-        #fv_SIFT       = fd_SIFT(image)
+        fv_zernike     = fd_zernike(image_mask)
         
-
+        #put desired descriptors in a stack
         global_feature = np.hstack([fv_histogram, fv_loc_bin_patt])
         #global_feature = np.hstack([fv_hu_moments])
         #global_feature = np.hstack([fv_haralick])
@@ -140,9 +134,6 @@ for training_name in train_labels:
 print("[STATUS] completed Global Feature Extraction...")
 
 
-# get the overall feature vector size
-print("[STATUS] feature vector size {}".format(np.array(global_features).shape))
-
 # encode the target labels
 le          = LabelEncoder()
 target      = le.fit_transform(labels)
@@ -150,18 +141,12 @@ target      = le.fit_transform(labels)
 scaler            = MinMaxScaler(feature_range=(0, 1))
 rescaled_features = scaler.fit_transform(global_features)
 
-# split the training and testing data
-#(trainDataGlobal, testDataGlobal, trainLabelsGlobal, testLabelsGlobal) = train_test_split(np.array(rescaled_features),
-#                                                                                          np.array(target),
-#                                                                                          test_size=test_size,
-#                                                                                          shuffle=True,
-#                                                                                          random_state=seed)
-                                                                                          
-#print("Train data  : {}".format(trainDataGlobal.shape))
-#print("Test data   : {}".format(testDataGlobal.shape))
-
-
+#initiate ML model
 clf  = RandomForestClassifier(n_estimators=num_trees, random_state=seed)
+
+#initiate 10-fold validation function
 kfold = KFold(n_splits=10, random_state=seed)
+
+#predict the images labels using model with input data and 10-fold validation
 cv_results = cross_val_score(clf, rescaled_features, target, cv=kfold, scoring=scoring)
 print(cv_results.mean())
